6b3e3ef02294f4b18c3f7bba80052537d405fef1
Luukas Lehtinen <lukituubi@gmail.com>
2022-06-11 17:46:34 +0300

    Create grid

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 
-function App() {
+const App = () => {
+  
+  const width = 5;
+  const height = 5;
+  const size = 10;
+
   return (
     <div>
-      Hello
-    </div>    
+      <svg viewBox="0 0 200 100" xmlns="http://www.w3.org/2000/svg" >
+        <rect width="100%" height="100%" fill="gray" />
+        {
+          [...new Array(width).keys()].map(x => (
+            [...new Array(height).keys()].map(y => (
+              <rect x={`${x*size}`} y={y*size} width={size} height={size} fill="none" stroke="black" />
+            ))
+          ))
+        }
+      </svg>
+    </div>
   );
 }
 
