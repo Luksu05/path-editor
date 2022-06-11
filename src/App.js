1a16ba63fd4e84026b74cda37029b266c90feb19
Luukas Lehtinen <lukituubi@gmail.com>
2022-06-11 19:10:54 +0300

    Draw current obstacle and store obstacles to array

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React, { useState } from "react";
 import Grid from "./Grid";
 
+let obstacles = [];
+
 const App = () => {
   const [pos, setPos] = useState({ x: 0, y: 0 });
+  const [obstacle, setObstacle] = useState([]);
   const width = 50;
   const height = 50;
-  const size = 10;
+  const size = 100;
 
   const handleMouse = (event) => {
     setPos({
       x: Math.round(event.pageX / size) * size,
       y: Math.round(event.pageY / size) * size,
     });
+  };
+
+  const handleClick = () => {
+    const [x, y] = [pos.x / size, pos.y / size];
+    if (obstacle.length && x === obstacle[0].x && y === obstacle[0].y) {
+      obstacles.push(obstacle);
+      setObstacle([]);
+    } else {
+      setObstacle([...obstacle, { x, y }]);
+    }
   };
 
   return (
@@ -22,9 +35,16 @@ const App = () => {
         height="800px"
         xmlns="http://www.w3.org/2000/svg"
         onMouseMove={handleMouse}
+        onClick={handleClick}
       >
         <rect width="100%" height="100%" fill="gray" />
         <Grid width={width} height={height} size={size} />
+        <polygon
+          points={obstacle
+            .map((pos) => `${pos.x * size},${pos.y * size}`)
+            .join(" ")}
+          fill="pink"
+        />
         <circle cx={pos.x} cy={pos.y} r="5" fill="red" />
       </svg>
     </div>
