94be1eb4e6e4dce58fd37ac7470bb92cbadcc168
Luukas Lehtinen <luukas.lehtinen@paivola.fi>
2022-08-12 13:38:55 +0300

    Adjust divs and add App.css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Grid from "./components/Grid";
 import Obstacles from "./components/Obstacles";
 import Obstacle from "./components/Obstacle";
+import "./App.css";
 
 let obstacles = [];
 
@@ -11,11 +12,17 @@ const App = () => {
   const width = 50;
   const height = 50;
   const size = 100;
+  const ref = useRef(null);
+
+  useEffect(() => {
+    console.log("width", ref.current.offsetWidth);
+    console.log("height", ref.current.offsetHeight);
+  }, []);
 
   const handleMouse = (event) => {
     setPos({
       x: Math.round(event.pageX / size) * size,
-      y: Math.round(event.pageY / size) * size,
+      y: Math.round((event.pageY - 70) / size) * size,
     });
   };
 
@@ -30,21 +37,26 @@ const App = () => {
   };
 
   return (
-    <div>
-      <svg
-        viewBox="0 0 800 800"
-        width="800px"
-        height="800px"
-        xmlns="http://www.w3.org/2000/svg"
-        onMouseMove={handleMouse}
-        onClick={handleClick}
-      >
-        <rect width="100%" height="100%" fill="gray" />
-        <Grid width={width} height={height} size={size} />
-        <Obstacles obstacles={obstacles} size={size} />
-        <Obstacle obstacle={obstacle} size={size} />
-        <circle cx={pos.x} cy={pos.y} r="5" fill="red" />
-      </svg>
+    <div className="maindiv">
+      <header>
+        <h1>{width}</h1>
+      </header>
+      <div ref={ref} className="innerdiv">
+        <svg
+          viewBox="0 0 800 800"
+          width={size * 8}
+          height={size * 8  }
+          xmlns="http://www.w3.org/2000/svg"
+          onMouseMove={handleMouse}
+          onClick={handleClick}
+        >
+          <rect width="100%" height="100%" fill="gray" />
+          <Grid width={width} height={height} size={size} />
+          <Obstacles obstacles={obstacles} size={size} />
+          <Obstacle obstacle={obstacle} size={size} />
+          <circle cx={pos.x} cy={pos.y} r="5" fill="red" />
+        </svg>
+      </div>
     </div>
   );
 };
