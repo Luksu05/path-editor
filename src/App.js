ef09761cfe75bab3f233c83c906c9cca914c77fe
Luukas Lehtinen <luukas.lehtinen@paivola.fi>
2022-08-12 14:29:18 +0300

    Center grid on screen

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useState } from "react";
 import Grid from "./components/Grid";
 import Obstacles from "./components/Obstacles";
 import Obstacle from "./components/Obstacle";
@@ -8,21 +8,18 @@ let obstacles = [];
 
 const App = () => {
   const [pos, setPos] = useState({ x: 0, y: 0 });
+  const [posOffset, setPosOffset] = useState({ x: 0, y: 0 });
   const [obstacle, setObstacle] = useState([]);
   const width = 50;
   const height = 50;
   const size = 100;
-  const ref = useRef(null);
-
-  useEffect(() => {
-    console.log("width", ref.current.offsetWidth);
-    console.log("height", ref.current.offsetHeight);
-  }, []);
+  const svg = document.getElementsByTagName("svg")[0];
 
   const handleMouse = (event) => {
+    getCoords(svg)
     setPos({
-      x: Math.round(event.pageX / size) * size,
-      y: Math.round((event.pageY - 70) / size) * size,
+      x: Math.round(event.pageX / size) * size - posOffset.y,
+      y: Math.round(event.pageY / size) * size - posOffset.x,
     });
   };
 
@@ -36,16 +33,34 @@ const App = () => {
     }
   };
 
+  const getCoords = (elem) => {
+    const box = elem.getBoundingClientRect();
+
+    const body = document.body;
+    const docEl = document.documentElement;
+
+    let scrollTop = window.pageYOffset || docEl.scrollTop || body.scrollTop;
+    let scrollLeft = window.pageXOffset || docEl.scrollLeft || body.scrollLeft;
+
+    let clientTop = docEl.clientTop || body.clientTop || 0;
+    let clientLeft = docEl.clientLeft || body.clientLeft || 0;
+
+    let top  = box.top +  scrollTop - clientTop;
+    let left = box.left + scrollLeft - clientLeft;
+
+    setPosOffset({x: Math.round(top / 100) * 100, y: Math.round(left / 100) * 100});
+  }
+
   return (
     <div className="maindiv">
       <header>
         <h1>{width}</h1>
       </header>
-      <div ref={ref} className="innerdiv">
+      <div className="innerdiv">
         <svg
           viewBox="0 0 800 800"
-          width={size * 8}
-          height={size * 8  }
+          width={800}
+          height={800}
           xmlns="http://www.w3.org/2000/svg"
           onMouseMove={handleMouse}
           onClick={handleClick}
