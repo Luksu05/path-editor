a6c8eb391e2fe52c18f47d503f6c416ba3386770
Luukas Lehtinen <lukituubi@gmail.com>
2022-08-18 09:34:36 +0300

    Change Point.js indent to 2 spaces

--- src/components/Point.js
+++ src/components/Point.js
@@ -1,13 +1,7 @@
 import React from "react";
 
 const Point = ({ pointX, pointY, size, color }) => (
-    <circle
-     cx={pointX * size}
-     cy={pointY * size}
-     r="10"
-     fill={color}
-    />
-  
+  <circle cx={pointX * size} cy={pointY * size} r="10" fill={color} />
 );
 
 export default Point;
