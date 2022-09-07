a9ad12c29a7871522f916aafb3675d87d96b07bd
Luukas Lehtinen <lukituubi@gmail.com>
2022-09-07 20:46:21 +0300

    Fix last random error in console

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 const Grid = ({ width, height, size }) => {
+  let index = 0;
   return (
     <React.Fragment>
       {[...new Array(width).keys()].map((x) =>
         [...new Array(height).keys()].map((y) => (
           <rect
+            key={index++}
             x={`${x * size}`}
             y={y * size}
             width={size}
