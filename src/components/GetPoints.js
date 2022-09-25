f72c5dfd0b4d0f1a92c55731285143481a668637
Luukas Lehtinen <lukituubi@gmail.com>
2022-09-25 18:57:31 +0300

    Make point names and graph two letters long

--- src/components/GetPoints.js
+++ src/components/GetPoints.js
@@ -1,15 +1,26 @@
+const getChr = (oldChr, index) => {
+  oldChr = oldChr.split('');
+
+  if (index % 25 === 0) {
+    oldChr[1] = String.fromCharCode(97 + (index % 25));
+  }
+  oldChr[1] = String.fromCharCode(97 + index);
+  return oldChr.join('');
+}
+
 const GetPoints = ({ obstacles }, { pointA }, { pointB }) => {
   let points = {};
-  points.a = pointA;
+  points.aa = pointA;
   let index = 1;
+  let chr = 'ab';
   for (let i = 0; i < obstacles.length; i++) {
     for (let j = 0; j < obstacles[i].length; j++) {
-      let chr = String.fromCharCode(97 + index);
+      chr = getChr(chr, index);
       points[chr] = obstacles[i][j];
       index++;
     }
   }
-  let chr = String.fromCharCode(97 + index);
+  chr = getChr(chr, index);
   points[chr] = pointB;
   return points;
 };
